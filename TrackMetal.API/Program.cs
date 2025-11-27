0ef5b69ddb88f0813aa7d1626e1f706f6cad6f6e
Popovici Andrei-Cristian <andrei-cristian.popovici@student.upt.ro>
2025-11-27 14:21:24 +0200

    Configured swagger

--- TrackMetal.API/Program.cs
+++ TrackMetal.API/Program.cs
@@ -26,6 +26,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Open Api");
+        options.RoutePrefix = "";
+    });
+
 }
 
 app.UseHttpsRedirection();
