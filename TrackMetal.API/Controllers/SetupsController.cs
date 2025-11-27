c4de0e83d410dbe1121069cbcb20e8cc1b3bad9d
afterforever17 <pbggabi@gmail.com>
2025-11-27 17:15:22 +0200

    Add basic setups controller

---
+++ TrackMetal.API/Controllers/SetupsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrackMetal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SetupsController : ControllerBase
+    {
+        public SetupsController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAllSetups()
+        {
+            return Ok();
+        }
+
+        [HttpGet, Route("{id}")]
+        public IActionResult GetSetupById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreateSetup()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateSetup()
+        {
+            return Ok();
+        }
+
+        [HttpDelete, Route("{id}")]
+        public IActionResult DeleteSetup(int id)
+        {
+            return Ok();
+        }
+    }
+}
