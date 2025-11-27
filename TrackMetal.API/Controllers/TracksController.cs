3a51f0795cb442a82ee0ca32b4ef1c28bbaca67f
afterforever17 <pbggabi@gmail.com>
2025-11-27 17:16:42 +0200

    Add basic track controller

---
+++ TrackMetal.API/Controllers/TracksController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrackMetal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TracksController : ControllerBase
+    {
+        public TracksController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAllTracks()
+        {
+            return Ok();
+        }
+
+        [HttpGet, Route("{id}")]
+        public IActionResult GetTrackById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTrack()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTrack()
+        {
+            return Ok();
+        }
+
+        [HttpDelete, Route("{id}")]
+        public IActionResult DeleteTrack(int id)
+        {
+            return Ok();
+        }
+    }
+}
