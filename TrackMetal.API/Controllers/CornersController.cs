ecfbe423e401d52822018fb4619eaedd3a6c5fac
afterforever17 <pbggabi@gmail.com>
2025-11-27 17:18:11 +0200

    Add basic corner controller

---
+++ TrackMetal.API/Controllers/CornersController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrackMetal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CornersController : ControllerBase
+    {
+        public CornersController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCorners()
+        {
+            return Ok();
+        }
+
+        [HttpGet, Route("{id}")]
+        public IActionResult GetCornerById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCorner()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCorner()
+        {
+            return Ok();
+        }
+
+        [HttpDelete, Route("{id}")]
+        public IActionResult DeleteCorner(int id)
+        {
+            return Ok();
+        }
+    }
+}
