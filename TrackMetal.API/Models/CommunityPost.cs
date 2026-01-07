273a059fc257465b3b9cad7654003c135a9aa940
afterforever17 <pbggabi@gmail.com>
2026-01-07 23:59:13 +0200

    Add the rest of the models

---
+++ TrackMetal.API/Models/CommunityPost.cs
@@ -0,0 +1,10 @@
+﻿namespace TrackMetal.API.Models
+{
+    public class CommunityPost
+    {
+        public int CommunityPostId { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+    }
+}
---
+++ TrackMetal.API/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace TrackMetal.API.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public int Role { get; set; }
+        public int SafetyRating { get; set; }
+        public int ExperienceRating { get; set; }
+    }
+}
